d74ac0c529277dfb010cb59aca48276b64a9b4b3
Shawn Ko Phor Shyuan <koshawnps@gmail.com>
2026-09-23 01:11:06 +0800

    add basic  welcoming and input codes

---
+++ SKmain.py
@@ -0,0 +1,5 @@
+print('Welcome to Airbnb filter! \n'
+      'Where we will recommend your suite tier based on your budget!')
+Total_people = float(input('How many people are going on the trip? '))
+Budget = float(input("What's your budget for the trip? "))
+
