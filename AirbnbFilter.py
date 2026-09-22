208297130150662e3263158ff227b6a4a31b2f60
Lai Wei Shen <laiweishen2007@gmail.com>
2026-09-22 15:31:12 +0800

    added the logic for one person filter

--- AirbnbFilter.py
+++ AirbnbFilter.py
@@ -2,10 +2,26 @@ import time
 
 print('Hi, Welcome to Airbnb rooms filter!')
 time.sleep(1)
-print("Are you looking for an Aiebnb that's suitable for your upcoming stay?")
+print("Are you looking for an Airbnb that's suitable for your upcoming stay?")
 time.sleep(1)
-
 Budget = float(input('What is your budget? '))
 time.sleep(1)
+Guest = int(input('How many guest are staying over? '))
 
-Rooms = int(input('How many rooms are you looking for? '))
+try:
+    if Guest == 1 :
+            if Budget < 100:
+                 room_type = "Shared room"
+                 room_desc = "A single bunk with a shared bathroom"
+            elif Budget <= 200:
+                 room_type = "Private room"
+                 room_desc = "Private room in a shared house with shared common areas"
+            elif Budget >= 200:
+                 room_type = "Studio"
+                 room_desc = "Full private unit with self check in, have private kitchen and bathroom."
+
+    print(f"Room Type = {room_type}")
+    print(f"Room Description = {room_desc}")
+
+except:
+    print("Please enter the correct numerical value for the questions")
