5bbfb8eb362ff02a0b28466d50a1dd64abafe19a
Shawn Ko Phor Shyuan <koshawnps@gmail.com>
2026-09-23 13:11:23 +0800

    add rules of budget tiers to number of people and tiers of accommodation.

--- SKmain.py
+++ SKmain.py
@@ -1,22 +1,24 @@
 from dataclasses import dataclass
 
+
+#dataclass for different accommodation types
 @dataclass
 class Tier :
     name: str
     description: str
     rating: float
 Tiers = {
-    Tier(
+    "shared": Tier(
         name = "Shared room",
         description = "A room where you will sleep with other people.",
         rating = 1.5
     ),
-    Tier(
+    "room": Tier(
         name = "Private Room",
         description = "A personal room that doesn't needed to be shared with others.",
         rating = 3.5
     ),
-    Tier(
+    "unit": Tier(
         name = "Entire housing unit",
         description = "A housing unit (e.g: an apartment or a bangalow) contains multiple private rooms.\n "
                       "You will not need to share with others too",
@@ -25,7 +27,20 @@ Tiers = {
     )
 }
 
+#----Rules-----
+#map minimum budget tier requirements to number of people
+Rules = {
+    "low":  (1, 2),
+    "mid":  (1, None),
+    "high": (1, None),
+}
 
+#Map budget tiers to accommodation tiers
+TIER_MAP = {
+    "low":  Tiers["shared"],
+    "mid":  Tiers["room"],
+    "high": Tiers["unit"]
+}
 
 print('Welcome to Airbnb filter! \n'
       'A place where we will recommend your suite tier based on your budget!')
