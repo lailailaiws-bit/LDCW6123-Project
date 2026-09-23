6dcfe9f2ea01ac58608ad033e82f31a9ac6ebe48
Shawn Ko Phor Shyuan <koshawnps@gmail.com>
2026-09-23 10:45:51 +0800

    make user input as function

--- SKmain.py
+++ SKmain.py
@@ -1,5 +1,10 @@
 print('Welcome to Airbnb filter! \n'
-      'Where we will recommend your suite tier based on your budget!')
-Total_people = float(input('How many people are going on the trip? '))
-Budget = float(input("What's your budget for the trip? "))
-
+      'A place where we will recommend your suite tier based on your budget!')
+def user_input():
+    try:
+        Total_people = float(input('How many people are going on the trip? '))
+        Budget = float(input("What's your budget for the trip (in RM)? "))
+    except ValueError:
+        print('Please enter a valid numerical number.')
+        user_input()
+user_input()
